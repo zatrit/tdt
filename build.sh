fce169d156b8b8b9788016c7e718ce2a3048aed2
zatrit <zatrit@gmail.com>
2020-11-07 14:59:55 +0300

    Renamed all hpp's to cpp's

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 clang++ *.cpp *.hpp
 rm -rf *.gch *.o
 if [ -f "a.out" ]; then
-mv a.out tdt
+mkdir bin
+mv a.out bin/tdt
 echo $(tput setaf 2)$(tput bold 1)Build completed$(tput bold 0)$(tput setaf 0)
 else
 echo $(tput setaf 1)$(tput bold 1)Build failed$(tput bold 0)$(tput setaf 0)
